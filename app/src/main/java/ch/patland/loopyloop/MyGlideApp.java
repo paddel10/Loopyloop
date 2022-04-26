11c8e023371035a22fbaf94373acbd66bc67f07e
patrick <preichar@gmx.net>
2022-04-26 11:06:21 +0200

    refactor: remove unnecessary file

--- app/src/main/java/ch/patland/loopyloop/MyGlideApp.java
+++
@@ -1,9 +0,0 @@
-package ch.patland.loopyloop;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class MyGlideApp extends AppGlideModule {
-
-}
