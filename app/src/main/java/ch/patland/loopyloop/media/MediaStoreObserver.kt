d691f3bd35062e6027d07be4d46aff520f76a6dc
patrick <preichar@gmx.net>
2022-04-27 09:14:52 +0200

    feat: updating list if something changes

--- app/src/main/java/ch/patland/loopyloop/media/MediaStoreObserver.kt
+++ app/src/main/java/ch/patland/loopyloop/media/MediaStoreObserver.kt
@@ -3,16 +3,16 @@ package ch.patland.loopyloop.media
 import android.database.ContentObserver
 import android.net.Uri
 import android.os.Handler
-import android.util.Log
 
-class MediaStoreObserver(handler: Handler): ContentObserver(handler) {
+class MediaStoreObserver(private val mediaStoreObserverInterface: MediaStoreObserverInterface,
+                         handler: Handler): ContentObserver(handler) {
 
     override fun onChange(selfChange: Boolean) {
         super.onChange(selfChange, null)
     }
 
     override fun onChange(selfChange: Boolean, uri: Uri?) {
-        // todo: do an update
-        Log.d("MediaStoreObserver", "there is a change: " + uri?.path)
+        // do an update
+        mediaStoreObserverInterface.updateMediaItems()
     }
 }
